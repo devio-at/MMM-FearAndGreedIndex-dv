a98e2cdf24c8e91e3cbaabfc1808517fefc0474d
devio-at <github@devio.at>
2025-02-09 11:48:54 +0100

    replace Python fearandgreed.py with nodejs fetch

---
+++ MMM-FearAndGreedIndex-dv.js
@@ -0,0 +1,109 @@
+Module.register("MMM-FearAndGreedIndex-dv", {
+    defaults: {
+        updateInterval: 3600000, // Update every hour
+        header: {}
+    },
+
+    start() {
+        this.fearAndGreedIndex = {};
+        this.cryptoFearAndGreedIndex = {};
+        console.log("start");
+        this.sendSocketNotification("CONFIG", this.config);
+    },
+
+    getDom() {
+        console.log("getDom() begin");
+        var wrapper = document.createElement("div");
+
+        // Create and append the header
+        var header = document.createElement("header");
+        header.innerHTML = "Fear and Greed Index";
+        wrapper.appendChild(header);
+
+        var table = document.createElement("table");
+        wrapper.appendChild(table);
+        var tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+
+        var createIndexDisplay = (index, title) => {
+            var rowWrapper = document.createElement("tr");
+            var indexWrapper = document.createElement("td");
+            indexWrapper.setAttribute("style", 'font-size: smaller;');
+            rowWrapper.appendChild(indexWrapper);
+            indexWrapper.innerHTML = title; // "<strong>" + title + "</strong>";
+            var valueElement = document.createElement("td");
+            valueElement.style.fontSize = "smaller";
+            rowWrapper.appendChild(valueElement);
+            
+            if (index.value) {
+                var indexValue = parseFloat(index.value).toFixed(2);
+                // Check for 'description' or 'value_classification' depending on index type
+                var indexCategory = index.text || "Loading...";
+                
+                valueElement.innerHTML = indexValue + " (" + indexCategory + ")";
+                valueElement.style.color = this.getColorForIndex(parseFloat(indexValue));
+            } else {
+                valueElement.innerHTML += ": Loading...";
+            }
+            return rowWrapper;
+        };
+
+        try {
+            // Append traditional index
+            if (this.fearAndGreedIndex) {
+                for(var key in this.fearAndGreedIndex) {
+                    const index = this.fearAndGreedIndex[key];
+                    tbody.appendChild(createIndexDisplay({ value: index.score, text: index.rating }, key.replace(/_/g, " ")));
+                }
+            }
+        }
+        catch(ex) {
+            console.error(ex);
+        }
+
+
+        try {
+            // Append crypto index
+            if (this.cryptoFearAndGreedIndex.value) {
+                tbody.appendChild(createIndexDisplay(
+                    { value: parseFloat(this.cryptoFearAndGreedIndex.value), text: this.cryptoFearAndGreedIndex.value_classification }, 
+                    "Crypto"));
+            }
+        }
+        catch(ex) {
+            console.error(ex);
+        }
+
+        console.log("getDom() end");
+        return wrapper;
+    },
+
+    // Helper function to interpolate color based on index value
+    getColorForIndex: function(value) {
+        if (value >= 75) {  // extreme greed
+            return "rgb(0, 255, 0)"; // Green
+        } else if (value >= 55) { // extreme greed
+            return "rgb(255, 255, 0)"; // Yellow
+        } else if (value <= 25) {   // extreme fear
+            return "rgb(255, 0, 0)"; // Red
+        } else if (value <= 45) {       // fear
+            return "rgb(255, 165, 0)"; // Orange
+        } else {
+            return "#999"; // Grey
+        }
+    },    
+
+    socketNotificationReceived: function(notification, payload) {
+        console.log("socketNotificationReceived '" + notification + "'");
+
+        if (notification === "INDEX_DATA") {
+            console.log("Received INDEX_DATA:", payload); // Confirm that this data is received
+            this.fearAndGreedIndex = payload;
+            this.updateDom();
+        } else if (notification === "CRYPTO_INDEX_DATA") {
+            console.log("Received CRYPTO_INDEX_DATA:", payload); // Confirm that this data is received
+            this.cryptoFearAndGreedIndex = payload;
+            this.updateDom();
+        }
+    },    
+});
--- node_helper.js
+++ node_helper.js
@@ -1,19 +1,52 @@
-var NodeHelper = require("node_helper");
-var fetch = require("node-fetch");
-var fs = require("fs");
-var path = require("path");
+const NodeHelper = require("node_helper");
+
+// https://github.com/node-fetch/node-fetch/discussions/1579
+const fetch = (url, init) => import('node-fetch').then(module => module.default(url, init));
+const tls = require('tls');
+const fs = require("fs");
+const path = require("path");
+
+// const debug = function(s) { };
+const debug = function(s) { console.log("[MMM-FearAndGreedIndex-dv] DEBUG " + s); };
+const log = function(s, data) { 
+    if (data !== undefined) {
+        console.log("[MMM-FearAndGreedIndex-dv] " + s, data);
+    } 
+    else {
+        console.log("[MMM-FearAndGreedIndex-dv] " + s); 
+    }
+};
+const logError = function(s, data) { 
+    if (data !== undefined) {
+        console.error("[MMM-FearAndGreedIndex-dv] ERROR " + s, data);
+    } 
+    else {
+         console.error("[MMM-FearAndGreedIndex-dv] ERROR " + s); 
+    }
+};
 
 module.exports = NodeHelper.create({
-    start: function() {
-        console.log("MMM-FearAndGreedIndex helper started...");
+    start() {
+        log("node_helper started...");
+
+        debug("fetch " + typeof(fetch));
+        debug(JSON.stringify(fetch));
+
         this.scheduleUpdate();
         this.scheduleCryptoUpdate();
     },
     
-    socketNotificationReceived: function(notification, payload) {
+    async socketNotificationReceived(notification, payload) {
+        debug("socketNotificationReceived " + JSON.stringify(notification));
+        debug("socketNotificationReceived " + JSON.stringify(payload));
+
         if (notification === "CONFIG") {
             this.config = payload;
-            console.log("[MMM-FearAndGreedIndex] Configuration received.", this.config);
+            log("Configuration received.", this.config);
+
+            if (!this.config.updateInterval) {
+                logError("updateInterval not set");
+            }
             this.scheduleUpdate(); // Update traditional index
             this.scheduleCryptoUpdate(); // Update crypto index
         }
@@ -21,30 +54,20 @@ module.exports = NodeHelper.create({
 
     scheduleUpdate: function() {
         var self = this;
-        self.sendIndexData();
-        setInterval(function() {
-            self.sendIndexData();
-        }, this.config.updateInterval);
-    },
 
-    sendIndexData: function() {
-        var filepath = path.resolve(__dirname, this.config.filename);
-        console.log("Attempting to send INDEX_DATA from:", filepath);
-        if (fs.existsSync(filepath)) {
-            fs.readFile(filepath, (err, data) => {
-                if (err) {
-                    console.error("Error reading data.json:", err);
-                    return;
-                }
-                console.log("Sending INDEX_DATA:", JSON.parse(data));
-                this.sendSocketNotification("INDEX_DATA", JSON.parse(data));
-            });
-        } else {
-            console.log("data.json not found at:", filepath);
-        }
+        if (!this.config) { return; }
+
+        var self = this;
+        self.fetchFearAndGreed();
+        setInterval(function() {
+            self.fetchFearAndGreed();
+        }, this.config.updateInterval);
     },
     
     scheduleCryptoUpdate: function() {
+
+        if (!this.config) { return; }
+
         var self = this;
         self.fetchCryptoIndex();
         setInterval(function() {
@@ -52,9 +75,98 @@ module.exports = NodeHelper.create({
         }, this.config.updateInterval);
     },
     
+    fetchFearAndGreed: function() {
+        var self = this;
+
+        debug("fetchFearAndGreed()");
+
+        const filename = path.resolve(__dirname, "graphdata.json");
+
+        if (fs.existsSync(filename)) {
+
+            debug("fetchFearAndGreed() graphdata.json exists");
+
+            const stats = fs.statSync(filename);
+            const seconds = (new Date().getTime() - stats.mtime);
+
+            if (seconds < this.config.updateInterval) {
+                fs.readFile(filename, (err, data) => {
+                    if (err) {
+                        logError("reading data.json:", err);
+                        return;
+                    }
+                    debug("Sending INDEX_DATA:");
+                    debug(data);
+                    this.sendSocketNotification("INDEX_DATA", JSON.parse(data));
+                });
+            }
+        }
+
+        debug("Attempting to fetch fear-and-greed data.");
+        var url = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata";
+
+        tls.DEFAULT_MIN_VERSION = 'TLSv1.3';
+        tls.DEFAULT_CIPHERS = 'TLS_AES_256_GCM_SHA384';
+
+        debug("tls set");
+
+        var headers = Object.assign({
+            'Host': 'production.dataviz.cnn.io',
+        }, this.config.fearandgreedHeaders);
+
+        // avoid "I'm a teapot. You're a bot."
+        fetch(url, { headers: headers })
+            .then(response =>  response.json())
+            .then(data => {
+                debug("graphdata is " + typeof(data));
+
+                if (data) {
+                    debug("Sending INDEX_DATA:");
+                    debug(Object.keys(data));
+                    self.sendSocketNotification("INDEX_DATA", data);
+                    fs.writeFile(filename, JSON.stringify(data), (err) => {
+                        if (err) {
+                            logError('writing file ' + filename);
+                            logError(err.message);
+                        }
+                    });
+                } else {
+                    logError("Unexpected fear-and-greed data format:", data);
+                }
+            })
+            .catch(error => {
+                logError("Error fetching fear-and-greed data: ", error);
+            });
+    },
+
     fetchCryptoIndex: function() {
         var self = this;
-        console.log("Attempting to fetch crypto index data.");
+
+        debug("fetchCryptoIndex()");
+
+        const filename = path.resolve(__dirname, "crypto.json");
+
+        if (fs.existsSync(filename)) {
+
+            debug("fetchCryptoIndex() crypto.json exists");
+
+            const stats = fs.statSync(filename);
+            const seconds = (new Date().getTime() - stats.mtime);
+
+            if (seconds < this.config.updateInterval) {
+                fs.readFile(filename, (err, data) => {
+                    if (err) {
+                        logError("Error reading data.json:", err);
+                        return;
+                    }
+                    debug("Sending CRYPTO_INDEX_DATA:");
+                    debug(data);
+                    this.sendSocketNotification("CRYPTO_INDEX_DATA", JSON.parse(data));
+                });
+            }
+        }
+
+        debug("Attempting to fetch crypto index data.");
         var url = "https://api.alternative.me/fng/";
 
         fetch(url)
@@ -62,14 +174,22 @@ module.exports = NodeHelper.create({
             .then(data => {
                 if (data && data.data && data.data.length > 0) {
                     var indexData = data.data[0];
-                    console.log("Sending CRYPTO_INDEX_DATA:", indexData);
+                    debug("Sending CRYPTO_INDEX_DATA:");
+                    debug(indexData);
                     self.sendSocketNotification("CRYPTO_INDEX_DATA", indexData);
+
+                    fs.writeFile(filename, JSON.stringify(indexData), (err) => {
+                        if (err) {
+                            logError('error writing file ' + filename);
+                            logError(err.message);
+                        }
+                    });
                 } else {
-                    console.error("Unexpected crypto index data format:", data);
+                    logError("Unexpected crypto index data format:", data);
                 }
             })
             .catch(error => {
-                console.error("Error fetching crypto index:", error);
+                logError("Error fetching crypto index:", error);
             });
     },
 });
